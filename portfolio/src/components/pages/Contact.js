dadacd9797cf956d77b3e45dd23a4f76bbaf5627
Megan Steblay <megan@steblay.net>
2021-09-09 15:18:01 -0500

    Tweaking Contact page

--- portfolio/src/components/pages/Contact.js
+++ portfolio/src/components/pages/Contact.js
@@ -6,9 +6,9 @@ import { validateEmail } from '../utils/helpers';
 function Form() {
   // Create state variables for the fields in the form
   // We are also setting their initial values to an empty string
-  const [email, setEmail] = useState('');
   const [userName, setUserName] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
 
   const handleInputChange = (e) => {
@@ -17,13 +17,13 @@ function Form() {
     const inputType = target.name;
     const inputValue = target.value;
 
-    // Based on the input type, we set the state of either email, username, and password
+    // Based on the input type, we set the state of either email, username, and message
     if (inputType === 'email') {
       setEmail(inputValue);
     } else if (inputType === 'userName') {
       setUserName(inputValue);
     } else {
-      setPassword(inputValue);
+      setMessage(inputValue);
     }
   };
 
@@ -36,40 +36,42 @@ function Form() {
       setErrorMessage('Email or username is invalid');
       // We want to exit out of this code block if something is wrong so that the user can correct it
       return;
-      // Then we check to see if the password is not valid. If so, we set an error message regarding the password.
+      // Then we check to see if the message is not valid. If so, we set an error message regarding the message.
     }
+    
     alert(`Hello ${userName}`);
 
     // If everything goes according to plan, we want to clear out the input after a successful registration.
     setUserName('');
-    setPassword('');
+    setMessage('');
     setEmail('');
   };
 
   return (
     <div>
-      <p>Hello {userName}</p>
       <form className="form">
-        <input
-          value={email}
-          name="email"
-          onChange={handleInputChange}
-          type="email"
-          placeholder="email"
-        />
         <input
           value={userName}
           name="userName"
           onChange={handleInputChange}
           type="text"
-          placeholder="username"
+          placeholder="Name"
         />
+
         <input
-          value={password}
-          name="password"
+          value={email}
+          name="email"
           onChange={handleInputChange}
-          type="password"
-          placeholder="Password"
+          type="email"
+          placeholder="Email"
+        />
+
+        <input
+          value={message}
+          name="message"
+          onChange={handleInputChange}
+          type="message"
+          placeholder="Message"
         />
         <button type="button" onClick={handleFormSubmit}>Submit</button>
       </form>
