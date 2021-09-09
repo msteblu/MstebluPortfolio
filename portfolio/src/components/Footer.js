a07a60ae14144167ff1342501bf63e2f10ea9e11
Megan Steblay <megan@steblay.net>
2021-09-09 15:19:45 -0500

    Working on footer

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import gitHub from './assets/GitHub.png';
+import linkedIn from './assets/LI.png';
 
 export default function Footer() {
   return (
     <footer>
+        <img src={gitHub} alt="GitHub Logo"></img>
+        <img src={linkedIn} alt="LinkedIn Logo"></img>
+        {/* <img href=".../public/assets/GitHub.png"></img> */}
         <p>Github</p>
         <p>LinkedIn</p>
         <p>StackOverflow</p>
