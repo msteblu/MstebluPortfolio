77f26ea738d84c22dfef8b583b46dd42cc4439aa
Megan Steblay <megan@steblay.net>
2021-09-09 15:19:59 -0500

    Working on projects for portfolio

--- portfolio/src/components/Project.js
+++ portfolio/src/components/Project.js
@@ -2,10 +2,22 @@ import React from 'react';
 
 export default function Project() {
   return (
-    <footer>
-        <p>Github</p>
-        <p>LinkedIn</p>
-        <p>StackOverflow</p>
-    </footer>
+              <div class="col">
+                <div class="card shadow-sm">
+                  <img src="./assets/images/Dashboard.gif" class="bd-placeholder-img card-img-top" width="100%" height="225" />
+
+                  <div class="card-body">
+                    <h4>Tech Blog</h4>
+                    <p class="card-text">This application provides a blog site where developers can publish blog posts and comment on others' blog posts. It provides users a space to discuss and learn with each other.</p>
+                    <div class="d-flex justify-content-between align-items-center">
+                      <div class="btn-group">
+                        <button type="button" class="btn btn-sm btn-outline-secondary extra-btn" onclick="window.location.href='https://fast-meadow-10211.herokuapp.com/';" >App</button>
+                        <button type="button" class="btn btn-sm btn-outline-secondary extra-btn" onclick="window.location.href='https://github.com/msteblu/TechBlog';" >Repo</button>
+                      </div>
+                      <small class="text-muted"></small>
+                    </div>
+                  </div>
+                </div>
+              </div>
   );
 }
