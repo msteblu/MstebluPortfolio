8d2e2bde8b2c1923fd1cd0b3eb3c9edb3ba1db53
Megan Steblay <megan@steblay.net>
2021-09-10 20:20:18 -0500

    Updating Portfolio section

--- portfolio/src/components/Project.js
+++ portfolio/src/components/Project.js
@@ -1,20 +1,20 @@
 import React from 'react';
 
-export default function Project() {
+export default function Project(props) {
   return (
-              <div class="col">
-                <div class="card shadow-sm">
-                  <img src="./assets/images/Dashboard.gif" class="bd-placeholder-img card-img-top" width="100%" height="225" />
+              <div className="col">
+                <div className="card shadow-sm">
+                  <img src={`${props.image}`} className="bd-placeholder-img card-img-top" width="100%" height="225" />
 
-                  <div class="card-body">
-                    <h4>Tech Blog</h4>
-                    <p class="card-text">This application provides a blog site where developers can publish blog posts and comment on others' blog posts. It provides users a space to discuss and learn with each other.</p>
-                    <div class="d-flex justify-content-between align-items-center">
-                      <div class="btn-group">
-                        <button type="button" class="btn btn-sm btn-outline-secondary extra-btn" onclick="window.location.href='https://fast-meadow-10211.herokuapp.com/';" >App</button>
-                        <button type="button" class="btn btn-sm btn-outline-secondary extra-btn" onclick="window.location.href='https://github.com/msteblu/TechBlog';" >Repo</button>
+                  <div className="card-body">
+                    <h4>{props.title}</h4>
+                    <p className="card-text">{props.description}</p>
+                    <div className="d-flex justify-content-between align-items-center">
+                      <div className="btn-group">
+                        <button type="button" className="btn btn-sm btn-outline-secondary extra-btn" onclick={props.app} >App</button>
+                        <button type="button" className="btn btn-sm btn-outline-secondary extra-btn" onclick={props.repo} >Repo</button>
                       </div>
-                      <small class="text-muted"></small>
+                      <small className="text-muted"></small>
                     </div>
                   </div>
                 </div>
--- portfolio/src/components/pages/Portfolio.js
+++ portfolio/src/components/pages/Portfolio.js
@@ -1,15 +1,70 @@
 import React from 'react';
 import Project from '../Project';
+// Images
+import Tech from '../assets/images/Dashboard.gif';
+import Trivia from '../assets/images/Trivialympics.gif';
+import Blitz from '../assets/images/Mountain.png';
+import Tracker from '../assets/images/Employee_Tracker.gif';
+import Commerce from '../assets/images/ECommerce.gif';
+import Weather from '../assets/images/weather.png';
+
+const projects = [
+  {
+    image: `${Tech}`,
+    title: 'Tech Blog',
+    description: `This application provides a blog site where developers can publish blog posts and comment on others' blog posts. It provides users a space to discuss and learn with each other.`,
+    app: `window.location.href='https://fast-meadow-10211.herokuapp.com/';`,
+    repo: `window.location.href='https://github.com/msteblu/TechBlog';`,
+  },
+  {
+    image: `${Trivia}`,
+    title: 'Trivialympics',
+    description: 'Trivia Olympics is an interactive full-stack application that allows you to log in to play a fun trivia game. Users compete to solve trivia questions and move across the board to win the game.',
+    app: `window.location.href='https://young-harbor-83683.herokuapp.com/';`,
+    repo: `window.location.href='https://github.com/msteblu/CodingLand';`,
+  },
+  {
+    image: `${Blitz}`,
+    title: 'Blitz and Chaos',
+    description: 'Blitz and Chaos is a text-based, interactive story (run by JavaScript) providing you, the user, with a chance to play through a life as a hero. The choices you make along the way change the experience and outcome of your story.',
+    app: `window.location.href='https://justinean.github.io/BlitzandChaos/';`,
+    repo: `window.location.href='https://github.com/msteblu/BlitzandChaos';`,
+  },
+  {
+    image: `${Tracker}`,
+    title: 'Employee Tracker',
+    description: `This is a command-line application that provides a solution for easily managing a company's employees. It allows employers to view and manage different aspects of the departments, roles, and employees in their company.`,
+    app: `window.location.href='https://www.awesomescreenshot.com/video/4416767?key=1689c0490c9d56ff02dba9aad1fd25e1';`,
+    repo: `window.location.href='https://github.com/msteblu/EmployeeTracker';`,
+  },
+  {
+    image: `${Commerce}`,
+    title: 'E-Commerce Backend',
+    description: 'This application provides the back end so that an e-commerce website and company can easily manage their products and have the technology to compete with other companies. It uses an Express.js API and the Sequelize package to interact with a MySQL database.',
+    app: `window.location.href='https://www.awesomescreenshot.com/video/4499834?key=e6b97f1ebf7ef168da3c5341bb964790';`,
+    repo: `window.location.href='https://github.com/msteblu/E-Commerce_BackEnd';`,
+  },
+  {
+    image: `${Weather}`,
+    title: 'Weather Dashboard',
+    description: 'This is a simple weather application. It was built in order to give users a way to check the weather and forecast for a specific city of their choice. It utilizes the OpenWeather API to provide up-to-date data and predictions.',
+    app: `window.location.href='https://msteblu.github.io/WeatherDashboard/';`,
+    repo: `window.location.href='https://github.com/msteblu/WeatherDashboard';`,
+  },
+];
 
 export default function Portfolio() {
   return (
     <div>
       <h1>Portfolio</h1>
-      <div class="album py-5">
-          <div class="container">
+      <div className="album py-5">
+          <div className="container">
       
-            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3"></div>
-            <Project />
+            <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+              {projects.map((work) => (
+                <Project image={work.image} title={work.title} description={work.description} app={work.app} repo={work.repo}/>
+              ))}
+            </div>
           </div>
       </div>
     </div>
