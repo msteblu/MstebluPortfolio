e417fcedd0b84cd8adf02051b80a0eacbb49cb89
Megan Steblay <megan@steblay.net>
2021-09-17 16:17:51 -0500

    Working on the Contact form

--- portfolio/src/components/pages/Contact.js
+++ portfolio/src/components/pages/Contact.js
@@ -1,15 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 // Here we import a helper function that will check if the email is valid
-import { validateEmail } from '../utils/helpers';
+import { validateEmail } from "../utils/helpers";
 
 function Form() {
   // Create state variables for the fields in the form
   // We are also setting their initial values to an empty string
-  const [userName, setUserName] = useState('');
-  const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
+  const [userName, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
   const handleInputChange = (e) => {
     // Getting the value and name of the input which triggered the change
@@ -18,13 +18,15 @@ function Form() {
     const inputValue = target.value;
 
     // Based on the input type, we set the state of either email, username, and message
-    if (inputType === 'email') {
+    if (inputType === "email") {
       setEmail(inputValue);
-    } else if (inputType === 'userName') {
+    } else if (inputType === "userName") {
       setUserName(inputValue);
     } else {
       setMessage(inputValue);
     }
+
+    setErrorMessage("");
   };
 
   const handleFormSubmit = (e) => {
@@ -33,18 +35,27 @@ function Form() {
 
     // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
     if (!validateEmail(email)) {
-      setErrorMessage('Email is invalid');
+      setErrorMessage("Email is invalid.");
       // We want to exit out of this code block if something is wrong so that the user can correct it
       return;
       // Then we check to see if the message is not valid. If so, we set an error message regarding the message.
     }
-    
+
     alert(`Hello ${userName}`);
 
     // If everything goes according to plan, we want to clear out the input after a successful registration.
-    setUserName('');
-    setMessage('');
-    setEmail('');
+    setUserName("");
+    setMessage("");
+    setEmail("");
+  };
+
+  const blurFunction = (e) => {
+    const { target } = e;
+    if (!target.value) {
+      setErrorMessage("Field is required.");
+    }
+
+    return;
   };
 
   return (
@@ -53,45 +64,61 @@ function Form() {
         <h1>Contact</h1>
 
         <div class="form-floating mb-3">
-          <input 
-            type="text" 
-            class="form-control" 
-            id="floatingInput" 
-            placeholder="Name" 
-            value={userName} 
-            onChange={handleInputChange}/>
-          <label for="floatingInput">Name:</label>
-        </div>
-
-        <div class="form-floating mb-3">
-          <input type="email" 
-          class="form-control" 
-          id="floatingInput" 
-          placeholder="name@example.com"
-          value={email}
-          onChange={handleInputChange}/>
-          <label for="floatingInput">Email address:</label>
-        </div>
-
-        <div class="form-floating mb-3">
-          <input type="text" 
-          class="form-control" 
-          id="floatingInput" 
-          placeholder="Message"
-          value={message}
-          onChange={handleInputChange}
+          <input
+            type="text"
+            class="form-control"
+            id="floatingName"
+            placeholder="Name"
+            name="userName"
+            value={userName}
+            onChange={handleInputChange}
+            onBlur={blurFunction}
           />
-          <label for="floatingInput">Message:</label>
+          <label for="floatingName">Name:</label>
         </div>
 
-        <small class="form-text text-muted">This is where errors appear.</small>
-        <button type="button" className="btn btn-sm btn-outline-secondary extra-btn" onClick={handleFormSubmit}>Submit</button>
-      </form>
-      {errorMessage && (
-        <div>
-          <p className="error-text">{errorMessage}</p>
+        <div class="form-floating mb-3">
+          <input
+            type="email"
+            class="form-control"
+            id="floatingEmail"
+            placeholder="name@example.com"
+            name="email"
+            value={email}
+            onChange={handleInputChange}
+            onBlur={blurFunction}
+          />
+          <label for="floatingEmail">Email address:</label>
         </div>
-      )}
+
+        <div class="form-floating mb-3">
+          <input
+            type="text"
+            class="form-control"
+            id="floatingMessage"
+            placeholder="Message"
+            name="message"
+            value={message}
+            onChange={handleInputChange}
+            onBlur={blurFunction}
+          />
+          <label for="floatingMessage">Message:</label>
+        </div>
+
+        {errorMessage && (
+          <div>
+            <p className="error-text">{errorMessage}</p>
+          </div>
+        )}
+        <button
+          type="button"
+          className="btn btn-sm btn-outline-secondary extra-btn"
+          onClick={handleFormSubmit}
+        >
+          Submit
+        </button>
+      </form>
+
       <div className="text-center">
         <p class="text-muted">Or contact me here:</p>
         <p class="text-muted">Email: megan@steblay.net</p>
@@ -102,5 +129,3 @@ function Form() {
 }
 
 export default Form;
-
-      
